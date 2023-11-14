4896c7c0ba0488d060c1fad7a887e80dfad838e4
virtualynx <eryandi.angga.pratama@gmail.com>
2023-11-14 11:17:35 +0700

    hides map in page cek-kendaraan

--- resources/views/transportasi/cek_kendaraan.blade.php
+++ resources/views/transportasi/cek_kendaraan.blade.php
@@ -5,10 +5,10 @@ Cek Kendaraan
 @endsection
 
 @section('page-head')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+{{-- <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin="">
-</script>
+</script> --}}
 
 <style>
     @media (max-width: 768px) {
@@ -242,15 +242,15 @@ Cek Kendaraan
 @section('page-footer')
 <script>
     // setMap([-1.269160, 116.825264]);
-    var map = L.map('map').setView([-1.269160, 116.825264], 16);
-    var markers = [];
+    // var map = L.map('map').setView([-1.269160, 116.825264], 16);
+    // var markers = [];
 
-    L.tileLayer(
-        'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }
-    ).addTo(map);
+    // L.tileLayer(
+    //     'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //         maxZoom: 19,
+    //         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    //     }
+    // ).addTo(map);
 
     function changeType() {
 
