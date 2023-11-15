6c868f513af63696b19217c5efcad2802d328b2b
nopalhilmy <naufal.farras.hilmy@gmail.com>
2023-11-15 16:04:44 +0700

    Style Fix : Add Spinner
    Style Fix : Add Spinner

--- resources/views/__template_base.blade.php
+++ resources/views/__template_base.blade.php
@@ -23,7 +23,101 @@
 	<link href="{{asset('dist/css/style_v_0_1.css')}}" rel="stylesheet" type="text/css">
 
 	<style>
-		.opacity70 {
+		.lds-spinner {
+			color: official;
+			display: inline-block;
+			position: relative;
+			width: 80px;
+			height: 80px;
+		}
+
+		.lds-spinner div {
+			transform-origin: 40px 40px;
+			animation: lds-spinner 1.2s linear infinite;
+		}
+
+		.lds-spinner div:after {
+			content: " ";
+			display: block;
+			position: absolute;
+			top: 3px;
+			left: 37px;
+			width: 6px;
+			height: 18px;
+			border-radius: 20%;
+			background: #fff;
+		}
+
+		.lds-spinner div:nth-child(1) {
+			transform: rotate(0deg);
+			animation-delay: -1.1s;
+		}
+
+		.lds-spinner div:nth-child(2) {
+			transform: rotate(30deg);
+			animation-delay: -1s;
+		}
+
+		.lds-spinner div:nth-child(3) {
+			transform: rotate(60deg);
+			animation-delay: -0.9s;
+		}
+
+		.lds-spinner div:nth-child(4) {
+			transform: rotate(90deg);
+			animation-delay: -0.8s;
+		}
+
+		.lds-spinner div:nth-child(5) {
+			transform: rotate(120deg);
+			animation-delay: -0.7s;
+		}
+
+		.lds-spinner div:nth-child(6) {
+			transform: rotate(150deg);
+			animation-delay: -0.6s;
+		}
+
+		.lds-spinner div:nth-child(7) {
+			transform: rotate(180deg);
+			animation-delay: -0.5s;
+		}
+
+		.lds-spinner div:nth-child(8) {
+			transform: rotate(210deg);
+			animation-delay: -0.4s;
+		}
+
+		.lds-spinner div:nth-child(9) {
+			transform: rotate(240deg);
+			animation-delay: -0.3s;
+		}
+
+		.lds-spinner div:nth-child(10) {
+			transform: rotate(270deg);
+			animation-delay: -0.2s;
+		}
+
+		.lds-spinner div:nth-child(11) {
+			transform: rotate(300deg);
+			animation-delay: -0.1s;
+		}
+
+		.lds-spinner div:nth-child(12) {
+			transform: rotate(330deg);
+			animation-delay: 0s;
+		}
+
+		@keyframes lds-spinner {
+			0% {
+				opacity: 1;
+			}
+
+			100% {
+				opacity: 0;
+			}
+		}
+		/* .opacity70 {
 			opacity: 70%;
 		}
 
@@ -84,7 +178,7 @@
 			0%   {transform:translateX(-50px);}
 			50%  {transform:translateX(50px);}
 			100% {transform:translateX(-50px);}
-		}
+		} */
 	</style>
 	
     @yield('page-head')
@@ -93,9 +187,7 @@
 	<!-- Preloader -->
 	<div class="preloader-it opacity70">
 		{{-- <div class="la-anim-1"></div> --}}
-		<div class="loading">
-			<span>Loading</span>
-		</div>
+		<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 	</div>
 	<!-- /Preloader -->
 
