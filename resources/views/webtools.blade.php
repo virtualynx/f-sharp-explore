d9ed88f73aa392154c4be43f669ee0339aa42623
virtualynx <eryandi.angga.pratama@gmail.com>
2023-11-23 12:58:27 +0700

    fix webtool's iframe base url

--- resources/views/webtools.blade.php
+++ resources/views/webtools.blade.php
@@ -16,7 +16,7 @@
                     <div class="panel-body pt-5">
                         <div class="row">
                             <div class="col-md-12">
-                                <iframe src="https://x0320.id/webtools/index.html" width="100%" height="600px" title="Webtools"></iframe>
+                                <iframe src="{{ url('webtools_content/index.html') }}" src="https://x0320.id/webtools/index.html" width="100%" height="600px" title="Webtools"></iframe>
                             </div>
                         </div>
                     </div>
